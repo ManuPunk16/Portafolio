062a3087f8ac5495bdd22911830b7a6b6e9a109c
Luis Hernandez <luisadrianhdzsoto@gmail.com>
2024-11-19 14:08:58 -0600

    Upgraded Angular v19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,21 +13,21 @@ import { Subscription } from 'rxjs';
 import { ThemeService } from './services/theme.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    FooterComponent,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatListModule,
-    RouterModule,
-    MatSlideToggleModule
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        RouterOutlet,
+        FooterComponent,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        RouterModule,
+        MatSlideToggleModule
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnDestroy, OnInit {
 
