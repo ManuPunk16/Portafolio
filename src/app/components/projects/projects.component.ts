9de75c58e1facf0a9e579e3866330ed6a6a985fe
Luis Hernandez <luisadrianhdzsoto@gmail.com>
2024-11-29 17:28:10 -0600

    Proyects module

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Cards } from '../../models/cards.model';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { NgFor } from '@angular/common';
 
 @Component({
     selector: 'app-projects',
     standalone: true,
-    imports: [],
+    imports: [
+      MatCardModule,
+      NgFor,
+      MatChipsModule,
+      RouterLink
+    ],
     templateUrl: './projects.component.html',
     styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit{
+
+  public cards = Cards.cards;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
---
+++ src/app/models/cards.model.ts
@@ -0,0 +1,37 @@
+export class Cards {
+  public static cards = [
+    {
+      title: 'One',
+      description: 'The Chihuahua is a Mexican breed of toy dog. It is named for the Mexican state of Chihuahua and is among the smallest of all dog breeds. It is usually kept as a companion animal or for showing.',
+      image: 'https://e1.pxfuel.com/desktop-wallpaper/330/345/desktop-wallpaper-blue-anime-aesthetic-kawaii-aesthetic-horizontal.jpg',
+      chips: {
+        one: 'Angular',
+        two: 'CSS',
+        three: 'Typescript'
+      },
+      route: '/projects/onetest'
+    },
+    {
+      title: 'Two',
+      description: 'The Chihuahua is a Mexican breed of toy dog. It is named for the Mexican state of Chihuahua and is among the smallest of all dog breeds. It is usually kept as a companion animal or for showing.',
+      image: 'https://e1.pxfuel.com/desktop-wallpaper/330/345/desktop-wallpaper-blue-anime-aesthetic-kawaii-aesthetic-horizontal.jpg',
+      chips: {
+        one: 'Angular',
+        two: 'CSS',
+        three: 'Typescript'
+      },
+      route: '/projects/twotest'
+    },
+    {
+      title: 'Three',
+      description: 'The Chihuahua is a Mexican breed of toy dog. It is named for the Mexican state of Chihuahua and is among the smallest of all dog breeds. It is usually kept as a companion animal or for showing.',
+      image: 'https://e1.pxfuel.com/desktop-wallpaper/330/345/desktop-wallpaper-blue-anime-aesthetic-kawaii-aesthetic-horizontal.jpg',
+      chips: {
+        one: 'Angular',
+        two: 'CSS',
+        three: 'Typescript'
+      },
+      route: '/projects/threetest'
+    }
+  ];
+}
